253a4c2c40929f55ba869f6d8d1f5139e80fa72a
Your Name <you@example.com>
2025-06-25 17:18:13 +0200

    test: names and values

--- test/basic_nix_language/syntax_learning.nix
+++ test/basic_nix_language/syntax_learning.nix
@@ -11,19 +11,126 @@
 
 { taskName ? "all" }:
 
-let
-  task1 = _: let
-    x = 1;
-    y = 2;
-  in x + y;
 
-  task2 = _: let
-    x = 1;
-    y = 2;
-  in x * y;
+let
+  # =======================================
+  # Example-DataSet
+  # ---------------------------------------
+  example = {
+    string = "hello";
+    integer = 1;
+    float = 3.141;
+    bool = true;
+    null = null;
+    list = [ 1 "two" false ];
+    attribute-set = {
+      a = "hello";
+      b = 2;
+      c = 2.718;
+      d = false;
+    };
+  };
+
+  ###################################################################
+  # Names and Values
+  ###################################################################
+  # =======================================
+  # let ... in ...
+  # ---------------------------------------
+  task1 = _:
+    let
+      x = 1;
+      y = 2;
+    in x + y;
+
+  # =======================================
+  # attribute set /list access
+  # ---------------------------------------
+  task2 = data:
+    {
+      "2a"= data.list; # output simple nested attribute
+      "2b"= builtins.elemAt data.list 0; # output element from list - only via builtin function possible
+      "2c"= data.attribute-set.a; # output even more nested attribute
+    };
+
+  # =======================================
+  # with ...; ...
+  # ---------------------------------------
+  task3 = data:
+    {
+      "3a"= with data.attribute-set; [ a b c d]; #
+    };
+
+
+  # =======================================
+  # inherit (...) ...
+  # ---------------------------------------
+  task4 = _:
+    let
+        # simple inherit
+        subtask1 = _:
+        let
+            a.x = 1;
+            a.y = 2;
+        in
+            {
+                inherit a;
+            };
+
+        # referenced inherit
+        subtask2 = _:
+        let
+            a.x = 1;
+            a.y = 2;
+        in
+            {
+                inherit (a) x y;
+            };
+    in
+    {
+        "4a" = subtask1 {};
+        "4b" = subtask2 {};
+    };
+
+  # =======================================
+  # String interpolation ${ ... }
+  # ---------------------------------------
+    task5 = _: {
+    "5a" =
+        let
+        name = "Nix";
+        in
+        "hello ${name}";
+
+    "5b" =
+        let
+        a = "no";
+        in
+        "A BIG ${a + " ${a + " ${a}"}"}";
+    };
+
+
+  # =======================================
+  # Intented strings and File System Paths skipped
+  # ---------------------------------------
+  # https://nix.dev/tutorials/nix-language#indented-strings
+
+  # =======================================
+  # Lookup Paths
+  # ---------------------------------------
+    task7 = _:
+    <nixpkgs/lib>;
+
+  # =======================================
+  #
+  # ---------------------------------------
 
   tasks = {
-    inherit task1 task2;
+    inherit task1;
+    task2 =  _: task2 example;
+    task3 =  _: task3 example;
+    inherit task4 task5;
+    inherit task7;
   };
 in
   if taskName == "all" then
